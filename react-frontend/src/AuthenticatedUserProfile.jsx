c2678b625c75b2346b423d5e21a3a0a2fd875261
Dielli Demjaha <diellidemjaha@gmail.com>
2024-06-06 10:34:16 +0200

    Update AuthenticatedUserProfile.jsx

--- react-frontend/src/AuthenticatedUserProfile.jsx
+++ react-frontend/src/AuthenticatedUserProfile.jsx
@@ -3,7 +3,6 @@
 import React, { useEffect, useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
 import Navbar from './NavBar';
-// import UserActivities from './UserActivities';
 import UserProfileActivities from './UserProfileActivities';
 import Profile from './Profile';
 import { useParams } from 'react-router-dom';
@@ -35,17 +34,11 @@ const AuthenticatedUserProfile = () => {
     });
 
     useEffect(() => {
-        // Optional: Handle loading or error states if needed
     }, [loading, error]);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
 
-    //   const { name, email, profile } = data.profile;
-
-    // const { profile } = data;
-    // const { user } = profile || {}; 
-    // const profilePicPath = profile?.profile_pic_path || 'No profile picture';
     const { user } = data || {};
     if (!user) {
         return <p>No user data found</p>;
@@ -58,7 +51,6 @@ const AuthenticatedUserProfile = () => {
 
     return (
         <>
-            {/* <Navbar /> */}
             <div className="container mt-4">
                 <div className="card">
                     <div className="card-body">
@@ -75,7 +67,6 @@ const AuthenticatedUserProfile = () => {
                                 <div className='btn btn-danger'>Update profile</div>
                             </a>
                         )}
-                        {/* <a href='/profileupdate'><div className='btn btn-danger'>Update profile</div></a> */}
                         <div className="mt-3">
                             <p className="mb-1">Name: {name}</p>
                             <p className="mb-1">Email: {email}</p>
